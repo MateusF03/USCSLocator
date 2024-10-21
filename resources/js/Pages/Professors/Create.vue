78c3ae57f8fafcbdd068cdce175ee9798a1ad809
Mateus Ferrari <mateus.ferrari@exata.it>
2024-10-21 12:15:01 -0300

    Add table components for UI

--- resources/js/Pages/Professors/Create.vue
+++ resources/js/Pages/Professors/Create.vue
@@ -1,1 +1,73 @@
-<script setup></script>
+<script setup>
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import { Input } from "@/Components/ui/input";
+import { Button } from "@/Components/ui/button";
+import InputError from "@/Components/InputError.vue";
+import { vMaska } from "maska/vue";
+
+import { useForm, Link } from "@inertiajs/vue3";
+
+const form = useForm({
+    name: "",
+    email: "",
+    discipline: "",
+    room: "",
+    time_range: "",
+});
+</script>
+
+<template>
+    <DefaultLayout title="Create">
+        <div class="flex items-center justify-center h-screen">
+            <form
+                @submit.prevent="
+                    form.post(route('professors.store'), {
+                        onSuccess: () => form.reset(),
+                    })
+                "
+            >
+                <div class="space-y-2">
+                    <div class="flex space-x-2">
+                        <div>
+                            <Input placeholder="Nome" v-model="form.name" />
+                            <InputError :message="form.errors.name" />
+                        </div>
+                        <div>
+                            <Input placeholder="Email" v-model="form.email" />
+                            <InputError :message="form.errors.email" />
+                        </div>
+                    </div>
+                    <div class="flex space-x-2">
+                        <div>
+                            <Input
+                                placeholder="Matéria"
+                                v-model="form.discipline"
+                            />
+                            <InputError :message="form.errors.discipline" />
+                        </div>
+                        <div>
+                            <Input placeholder="Sala" v-model="form.room" />
+                            <InputError :message="form.errors.room" />
+                        </div>
+                    </div>
+                    <div>
+                        <Input
+                            placeholder="Horário"
+                            v-model="form.time_range"
+                            v-maska="'##:##-##:##'"
+                        />
+                        <InputError :message="form.errors.time_range" />
+                    </div>
+                </div>
+                <div class="flex justify-center mt-4 space-x-2">
+                    <Button type="submit">Cadastrar</Button>
+                    <Link :href="route('professors.index')">
+                        <Button type="button" @click="form.reset()">
+                            Cancelar
+                        </Button>
+                    </Link>
+                </div>
+            </form>
+        </div>
+    </DefaultLayout>
+</template>
--- resources/js/Pages/Professors/Index.vue
+++ resources/js/Pages/Professors/Index.vue
@@ -1,7 +1,61 @@
 <script setup>
 import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/Components/ui/table";
+import { Link } from "@inertiajs/vue3";
+import { Button } from "@/Components/ui/button";
+defineProps(["professors"]);
 </script>
 
 <template>
-    <DefaultLayout title="Index"> carlos </DefaultLayout>
+    <DefaultLayout title="Index">
+        <!-- create button -->
+        <div class="flex justify-end p-2">
+            <Link :href="route('professors.create')">
+                <Button>Create</Button>
+            </Link>
+        </div>
+        <div class="p-0 sm:p-6 lg:p-8">
+            <Table class="bg-white shadow-sm rounded-md">
+                <TableCaption>Professors</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Photo</TableHead>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Email</TableHead>
+                        <TableHead>Discipline</TableHead>
+                        <TableHead>Room</TableHead>
+                        <TableHead>Time Range</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow
+                        v-for="professor in professors"
+                        :key="professor.id"
+                        @click="console.log(professor)"
+                        class="cursor-pointer"
+                    >
+                        <TableCell>
+                            <img
+                                :src="professor.profile_photo_url"
+                                alt="profile photo"
+                            />
+                        </TableCell>
+                        <TableCell>{{ professor.name }}</TableCell>
+                        <TableCell>{{ professor.email }}</TableCell>
+                        <TableCell>{{ professor.discipline }}</TableCell>
+                        <TableCell>{{ professor.room }}</TableCell>
+                        <TableCell>{{ professor.time_range }}</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    </DefaultLayout>
 </template>
